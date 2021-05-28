11adcd790e6c6514e20220b7c06208efff5afa67
funloader <78745591+funloader@users.noreply.github.com>
2021-05-28 11:29:29 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+# A variable with in Integer (whole number) value
+my_age = 15
+# A variable with a String (text) value
+my_name = "Sam"
+
+# We can print out the contents of variables in a couple of ways:
+print(my_age)
+print("Hello " + my_name + "!")
+print("Hello {}, you are {} years old".format(my_name, my_age))
+
+a = 5
+b = 10
+c = a + b 
+print("a plus b = {}".format(c))
+# Experiment with +, -, / (division) and * (multiplication)!
