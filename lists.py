2ac723795875f2c80871559e7f89b6da6387217f
funloader <78745591+funloader@users.noreply.github.com>
2021-05-28 11:25:05 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Creates an empty list
+names = []
+
+# Creates a list with values
+planets = ["Mercury", "Venus", "Earth", "Mars"]
+planets.append("Jupiter")
+planets.append("Saturn")
+
+for planet in planets:
+  print(planet)
