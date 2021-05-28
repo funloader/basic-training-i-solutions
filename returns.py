cbf66580ad40ccec6365776b5d1487487f69edab
funloader <78745591+funloader@users.noreply.github.com>
2021-05-28 11:28:53 +0530

    Create returns.py

---
+++ returns.py
@@ -0,0 +1,6 @@
+def create_full_name(first_name, last_name):
+  full_name = first_name.upper() + " " + last_name.upper()
+  return full_name
+
+result = create_full_name("Neil", "Armstrong")
+print(result)
